ac221cc14762ead3ebff8b26d888bf5a4563f6ce
KlassiC-Man <parth.11.gulati@gmail.com>
2021-07-27 17:45:58 +0530

    dependecies installed

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ-r5aCFhtXtbxkKl8mdG6VtUruptlm1E",
+    authDomain: "crash-44799.firebaseapp.com",
+    projectId: "crash-44799",
+    storageBucket: "crash-44799.appspot.com",
+    messagingSenderId: "1082237792828",
+    appId: "1:1082237792828:web:3266ff4d40db8494bfc939",
+    measurementId: "G-7ELGQ0L7X7"
+  };
+
+const db = firebase.firestore();
+const auth = app.auth();
+  
+export {db, auth};
